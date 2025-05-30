a89e9c1d3e75bc83f902c05767facc27f847eb68
22060 <108120783+22060@users.noreply.github.com>
2025-05-30 09:44:37 +0900

    a

--- src/nlp/DfCompare.java
+++ src/nlp/DfCompare.java
@@ -3,12 +3,12 @@ package nlp;
 public class DfCompare implements java.util.Comparator<DfCount> {
     @Override
     public int compare(DfCount wc1, DfCount wc2) {
-        if (wc1.getIdf() < wc2.getIdf())
-            return 1;
-        if (wc1.getIdf() == wc2.getIdf())
-            return 0;
-        if (wc1.getIdf() > wc2.getIdf())
+        if (wc1.getCount() < wc2.getCount())
             return -1;
+        if (wc1.getCount() == wc2.getCount())
+            return 0;
+        if (wc1.getCount() > wc2.getCount())
+            return 1;
         return 0;
     }
 
--- src/nlp/NaturalLanguageProcessing.java
+++ src/nlp/NaturalLanguageProcessing.java
@@ -2,17 +2,17 @@ package nlp;
 
 public class NaturalLanguageProcessing {
     static public void main(String args[]) {
-        System.out.println("TF 導出!");
+        System.out.println("TF 導出");
         TermFrequency[] tf = new TermFrequency[100];
         DocumentFrequency df = new DocumentFrequency(tf);
         df.df("data\\df.txt");
-        // for (int i = 1; i <= 100; i++) {
-        // tf[i - 1] = new TermFrequency();
-        // String inputFileName = "data\\" + String.format("%03d", i) + ".txt";
-        // String outputFileName = "data\\" + String.format("%03d", i) + "tf.txt";
-        // // System.out.println(inputFileName);
-        // // System.out.println(outputFileName);
-        // tf[i - 1].tf(inputFileName, outputFileName);
-        // }
+        for (int i = 1; i <= 100; i++) {
+            tf[i - 1] = new TermFrequency();
+            String inputFileName = "data\\" + String.format("%03d", i) + ".txt";
+            String outputFileName = "data\\" + String.format("%03d", i) + "tf.txt";
+            // System.out.println(inputFileName);
+            // System.out.println(outputFileName);
+            tf[i - 1].tf(inputFileName, outputFileName);
+        }
     }
 }
